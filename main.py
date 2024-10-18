5887cd8992ceeb488a19bef2f54de44c71c34006
Yc3t <ycetaibi@gmail.com>
2024-10-18 13:40:44 +0200

    modularized knn and linear regression on main,plot linear regression

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import numpy as np
 from linear_regression import LinearRegression
+import matplotlib.pyplot as plt
 
 def accuracy(y_true,y_pred):
     total_predictions = len(y_true)
     accuracy = np.sum(y_true == y_pred) / total_predictions
     return accuracy
+
+
+def mse(y_true,y_predicted):
+    return np.mean((y_true-y_predicted)**2)
+    
+
+
 
 
 def knn():
@@ -23,15 +31,25 @@ def knn():
 
 
 
-
-
-
 def linear_regression():
-    pass
+    X,y = datasets.make_regression(n_samples=100,n_features=1,noise=20,random_state=2)
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=32)
+    regressor = LinearRegression(lr=0.01)
+    regressor.fit(X_train,y_train)
+    predicted = regressor.predict(X_test)
+    mse_value = mse(y_test,predicted)
+    print(mse_value)
+    y_pred_line = regressor.predict(X)
+    cmap = plt.get_cmap("viridis")
+    plt.plot(X, y_pred_line, color="black", linewidth=2, label="Prediction")
+    plt.show()
+
+
 
 
 def main():
-    knn()
+    #knn()
+    linear_regression()
         
 
 
