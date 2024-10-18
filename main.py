9ae4ad05f95e908f523394f8bfbd9d22c2ca49ab
Yc3t <ycetaibi@gmail.com>
2024-10-18 13:28:14 +0200

    edit name linear regression

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from knn import KNN
-from matplotlib.colors import ListedColormap
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import numpy as np
-
+from linear_regression import LinearRegression
 
 def accuracy(y_true,y_pred):
     total_predictions = len(y_true)
-    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    accuracy = np.sum(y_true == y_pred) / total_predictions
     return accuracy
 
-def main():
+
+def knn():
     iris = datasets.load_iris()
     X,y = iris.data, iris.target
 
@@ -21,7 +21,18 @@ def main():
     predictions = model.predict(X_test)
     print("KNN classification accuracy", accuracy(y_test, predictions))
 
-    
+
+
+
+
+
+def linear_regression():
+    pass
+
+
+def main():
+    knn()
+        
 
 
 if __name__ == "__main__":
